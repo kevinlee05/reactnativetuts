7a3d81c17ef2aa571622fe923f472ce08b424679
unknown <kevinlee05@gmail.com>
2018-04-19 02:23:55 +0800

    first component

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,15 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import a library to help create a component
+import React from 'react';
+import ReactNative, { Text, AppRegistry } from 'react-native';
+
+
+// Create a component
+const App = () => {
+    return (
+        <Text>Some Text </Text>
+    );
+};
+
+// Render the component to the device  
 
 AppRegistry.registerComponent('albums', () => App);
