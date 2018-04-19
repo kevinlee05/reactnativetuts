08397fd37b896773a81656c32cc8779bb6852fbc
unknown <kevinlee05@gmail.com>
2018-04-20 01:44:56 +0800

    add styling to view component

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,19 +1,37 @@
 // import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
-
+import { Text, View } from 'react-native';
+// View tag is used to position elements
 
 // make a component
 const Header = () => {
-    const { textStyle } = styles;
+    const { textStyle, viewStyle } = styles;
 
-    return <Text style={textStyle}>Albums App!</Text>;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums App!</Text>
+        </View>
+        
+    );
 };
 
 
 const styles = {
+    viewStyle :{
+        backgroundColor: 'yellow',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        paddingBottom: 15,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.9, //darkness of shadow
+        elevation: 2,
+        position: 'relative',
+    },
     textStyle:{
-        fontSize: 30
+        fontSize: 30,
     }
 }
 
