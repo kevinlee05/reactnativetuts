f4c218c8862d7a482ca215a4d4243bb153a23b31
unknown <kevinlee05@gmail.com>
2018-04-21 17:21:35 +0800

    button styling

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -3,13 +3,36 @@ import { Text, TouchableOpacity } from 'react-native';
 //touchable opacity is a button class
 
 const Button = () => {
+    const { buttonStyle, textStyle } = styles;
+
     return (
-        <TouchableOpacity>
-        <Text>
-            Click Me!!!
-        </Text>
+        <TouchableOpacity style={buttonStyle}>
+            <Text style={textStyle}>
+                Click Me!!!
+            </Text>
         </TouchableOpacity>
     );
+}
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    buttonStyle: {
+        flex: 1, //expand to fill content space
+        alignSelf: 'stretch', //stretch to fill container
+        bakgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff', //ios blue
+        marginLeft: 5,
+        marginRight: 5
+    }
 }
 
 export default Button;
