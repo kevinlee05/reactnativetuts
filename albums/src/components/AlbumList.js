06ab32df2ac722e46a7594cae711ef25c49ed500
unknown <kevinlee05@gmail.com>
2018-04-21 12:19:19 +0800

    initialize AlbumList state and use this.setState to update component after receipt of API data

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,13 +3,16 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] }; //initialize state to an empty albums array
+
     componentWillMount() {
-        axios
-        .get('https://rallycoding.herokuapp.com/api/music_albums')
-        .then(response => console.log(response));
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({albums: response.data})); //update component state with the api data
+        // do not use this.state = {}
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album List!!!</Text>
