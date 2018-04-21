d6493f37b2b94840f7d58070fd56279dc51c4aae
unknown <kevinlee05@gmail.com>
2018-04-21 23:37:43 +0800

    initialize firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {  };
     }
+    componentWillMount(){
+        firebase.initializeApp({
+            apiKey: "AIzaSyCGxxUO9Wph8UZ9Oom2b_P-d3q0AJSJ8e8",
+            authDomain: "authapp-4068f.firebaseapp.com",
+            databaseURL: "https://authapp-4068f.firebaseio.com",
+            projectId: "authapp-4068f",
+            storageBucket: "authapp-4068f.appspot.com",
+            messagingSenderId: "405604740881"
+        });
+    }
     render() {
         return (
-            
             <View>
                 <Header headerText="Auth App" />
                 <Text>Auth App</Text>
